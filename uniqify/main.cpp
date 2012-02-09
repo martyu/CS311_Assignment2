9b61a45e0e6bd266fbd7e68d74c797b8ab1b338e
Marty Ulrich <martyulrich@earthlink.net>
2012-02-08 18:05:12 -0800

    Finally got the word parser parsing correctly.  Note to self: LEARN C++!

--- uniqify/main.cpp
+++ uniqify/main.cpp
@@ -1,15 +1,42 @@
-//
-//  main.cpp
-//  uniqify
-//
-//  Created by Marty Ulrich on 2/7/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
 
 #include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <vector>
+#include <string.h>
 
-int main (int argc, const char * argv[])
-{
+#define MAX_WORD_LENGTH 30 // longest english word length
+
+void parse_words(std::vector<std::string> *temp_words_vec);
+
+int main (int argc, char * argv[])
+{	
+//	int num_of_proc = atoi(argv[1]);
+		
+	std::vector<std::string> words_vector;
+
+	parse_words(&words_vector);
 	
+	for (std::vector<std::string>::iterator i = words_vector.begin();
+		 i != words_vector.end();
+		 ++i)
+	{
+		std::cout << *i << "\n";
+	}
+	
+	return 0;
 }
 
+
+void parse_words(std::vector<std::string> *temp_word_vec)
+{
+	char temp_word[MAX_WORD_LENGTH];
+	
+	while(std::fgets(temp_word, MAX_WORD_LENGTH, stdin))
+	{
+		for(int i = 0; i < strlen(temp_word); i++)
+			temp_word[i] = tolower(temp_word[i]);
+				
+		word_vec->push_back(temp_word);
+	}
+}
