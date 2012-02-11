73c195bde78e63ae0700b3d92779a118ebb89e0e
Marty Ulrich <martyulrich@earthlink.net>
2012-02-10 21:36:45 -0800

    Made pipes to connect to sort function, making function to write to these pipes.

--- uniqify/main.cpp
+++ uniqify/main.cpp
@@ -14,131 +14,120 @@ using namespace std; // TAKE THIS OUT!!!
 
 typedef int fd_t; // file descriptor type
 
-int parse_words(vector< vector<string> > &temp_word_vec);
+int parse_words(vector< vector<string> > &temp_word_vec, int num_of_sub_lists);
 vector<string>* merge_sort(vector<string> *temp_words_vec);
-int write_to_pipe(int pipe_file_desc);
-int read_from_pipe(int pipe_file_desc);
+int write_to_sort(fd_t pipes_to_sort[][2], int num_of_pipes);
+int read_from_sort(fd_t pipes_to_parent[][2], int num_of_pipes);
 
 int main (int argc, char * argv[])
-{	
+{
+	
 	int num_of_proc = atoi(argv[1]);
-	fd_t fd_arr[num_of_proc];
-	fd_t parent_to_child_pipe[2];
-	fd_t child_to_parent_pipe[2];
+	fd_t pipes_to_parent[num_of_proc][2];
+	fd_t pipes_to_sort[num_of_proc][2];
 	vector< vector<string> > word_vec(num_of_proc, vector<string>());
 	
-	if(pipe(parent_to_child_pipe) == -1 || pipe(child_to_parent_pipe) == -1)
-	{
-		cout << stderr << "Pipe failed. Exiting\n";
-		return -1;
-	}
+	parse_words(word_vec, num_of_proc);
 	
-	parse_words(word_vec);
-		
+	for(int j = 0; j < 3; j++)
+	{
+		vector<string> theVec = word_vec[j];
+		cout << "vector number " << j;
+		for (vector<string>::iterator i = theVec.begin(); i != theVec.end(); ++i)
+			std::cout << *i << " ";
+		cout << "\n";
+	}
+
+	
 	for(int i = 0; i < num_of_proc; i++)
 	{
-		if((fd_arr[i] = fork()) == -1)
+		if (pipe(pipes_to_sort[i]) == -1)
 		{
 			return -1;
 		}
-		
-		if(fd_arr[i] == 0) //child process
+		switch (fork())
 		{
-			if((close(parent_to_child_pipe[0])) == -1)
+			case -1:
 				return -1;
-			   
-			if((close(child_to_parent_pipe[1])) == -1)
-				return -1;
-			
-			break;
+				break;
+				
+			case 0: //child process
+				if (pipes_to_parent[i][0] != STDOUT_FILENO)
+				{
+					if (dup2(pipes_to_parent[i][0], STDOUT_FILENO) == -1)
+					{
+						return -1;
+					}
+					close(pipes_to_parent[i][0]);
+				}
+				if (pipes_to_sort[i][1] != STDIN_FILENO)
+				{
+					if (dup2(pipes_to_sort[i][1], STDIN_FILENO) == -1)
+					{
+						return -1;
+					}
+					close(pipes_to_sort[i][1]);
+				}
+				execl("bin/sort", "sort");
+				break;
+		
+			default:	//parent process
+				close(pipes_to_sort[i][1]);
+				close(pipes_to_parent[i][0]);
+				write_to_sort(pipes_to_sort, num_of_proc);
+				while (wait(NULL) != -1)
+					continue;
+				if (errno != ECHILD)
+				{
+					return -1;
+				}
+				break;
 		}
+				
 	}
 	
-	
-	
-	
-	
-	
-	/*
-	std::vector<std::string> words_vector;
-	parse_words(&words_vector);
-	merge_sort(&words_vector);
-	
-	for (vector<string>::iterator i = right_half_words.begin(); i != right_half_words.end(); ++i)
-	std::cout << *i;	
-	*/
-	
+		
 	return 0;
 }
 
 
 
-int parse_words(vector< vector<string> > &temp_word_vec)
+int parse_words(vector< vector<string> > &temp_word_vec, int num_of_sub_lists)
 {
-	char word[3];
+	char word[15];//CHANGE TO STRING!#$%#$%#@$%@#$%
 	int proc_index = 0; //index of word arr for child process "proc_index"
 	
 	while (fscanf (stdin, "%s", &word) != EOF)
 	{
 		temp_word_vec[proc_index++].push_back(word);
-		if(proc_index == 3)
+		if(proc_index == num_of_sub_lists)
 			proc_index = 0;
 	}
 	
 	return 0;
 }
 
-/*
-vector<string>* merge_sort(vector<string> *temp_words_vec)
+
+int write_to_sort(fd_t pipes_to_sort[][2], int num_of_pipes)
 {
-	if(temp_words_vec->size() <= 1){
-		return temp_words_vec;
-	}
+	FILE *pipe_write_to_sort[num_of_pipes][2];
 	
-	vector<string> left_half_words;
-	vector<string> right_half_words;
-	vector<string>::iterator halfway_iter = temp_words_vec->begin() + temp_words_vec->size()/2;
-	left_half_words.assign(temp_words_vec->begin(), halfway_iter);
-	right_half_words.assign(halfway_iter, temp_words_vec->end());
-	
-	pid_t left_child_pid;
-	pid_t right_child_pid;
-	int parent_child_pipe[2];
-	
-	if (pipe(parent_child_pipe))
+	for(int i = 0; i < num_of_pipes; i++)
 	{
-		cout << stderr << "Pipe failed. Exiting\n";
-		exit(EXIT_FAILURE);
+		pipe_write_to_sort[i][0] = fdopen(pipes_to_sort[i][0], "w");
 	}
-	
-	left_child_pid = fork();
-	if(left_child_pid != 0){
-		right_child_pid = fork();
-	}
-		
-	if(left_child_pid == -1 || right_child_pid == -1)
+	while (true)
 	{
-		cout << strerror(errno);
-		exit(EXIT_FAILURE);
+		numRead = read(pfd[0], buf, BUF_SIZE);
+		if (numRead == -1)
+			errExit("read");
+		if (numRead == 0)
+		break; /* End-of-file */ if (write(STDOUT_FILENO, buf, numRead) != numRead)
+			fatal("child - partial/failed write");)
 	}
 	
-	switch (left_child_pid){
-		case 0: //left child process
-			clo
-			merge_sort(left_half_words);
-		default:
-			break;
-	}
 	
-	return NULL;
+	return 0;
 }
 
 
-int write_to_pipe(int pipe_file_desc)
-{
-	FILE *pipe_in_stream = fdopen(<#int#>, <#const char *#>)
-}
-
-
-
-*/
