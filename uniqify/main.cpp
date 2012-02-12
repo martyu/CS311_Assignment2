9ddd1932512fdf49ce8de2775fb2017adc8dbfe0
Marty Ulrich <martyulrich@earthlink.net>
2012-02-11 21:15:40 -0800

    Fixed a bug.  For pipes, 0 = OUT, 1 = IN!!!

--- uniqify/main.cpp
+++ uniqify/main.cpp
@@ -19,7 +19,7 @@ using namespace std; // TAKE THIS OUT!!!
 typedef int fd_t; // file descriptor type
 
 int parse_words(vector< vector<string> > &temp_word_vec, int num_of_sub_lists);
-int write_to_sort(fd_t pipes_to_sort[][2], vector< vector<string> > &temp_word_vec);
+int write_to_sort(fd_t pipes_to_sort[][2], vector< vector<string> > temp_word_vec);
 int read_from_sort(fd_t pipes_to_parent[][2], int num_of_pipes);
 
 int main (int argc, char * argv[])
@@ -34,7 +34,7 @@ int main (int argc, char * argv[])
 	for(int j = 0; j < num_of_proc; j++)
 	{
 		vector<string> theVec = word_vec[j];
-		cout << "vector number " << j;
+		cout << "vector number " << j << ": ";
 		for (vector<string>::iterator i = theVec.begin(); i != theVec.end(); i++)
 			std::cout << *i << " ";
 		cout << "\n";
@@ -53,6 +53,9 @@ int main (int argc, char * argv[])
 			return -1;
 		}
 		
+		cout << "sort pipes " << i << ": \t\t" << pipes_to_sort[i][0] << "\t" << pipes_to_sort[i][1] << "\n";
+		cout << "parent pipes " << i << ": \t" << pipes_to_parent[i][0] << "\t" << pipes_to_parent[i][1] << "\n";
+		
 		switch (fork())
 		{
 			case -1:
@@ -61,35 +64,37 @@ int main (int argc, char * argv[])
 				break;
 				
 			case 0: //child process
+				close(pipes_to_parent[i][1]);
+				close(pipes_to_sort[i][0]);
 				if (pipes_to_parent[i][0] != STDOUT_FILENO)
 				{
-					cout << "point1\n";
+//					cout << "point1\n";
 					if (dup2(pipes_to_parent[i][0], STDOUT_FILENO) == -1)
 					{
 						cout << "dup2 error 1: " << strerror(errno) << "\n";
 						return -1;
 					}
-					cout << "point2\n";
+//					cout << "point2\n";
 					close(pipes_to_parent[i][0]);
 				}
 				if (pipes_to_sort[i][1] != STDIN_FILENO)
 				{
-					cout << "point3\n";
+//					cout << "point3\n";
 					if (dup2(pipes_to_sort[i][1], STDIN_FILENO) == -1)
 					{
 						cout << "Pipe error 2: " << strerror(errno) << "\n";
 						return -1;
 					}
-					cout << "point4\n";
+//					cout << "point4\n";
 					close(pipes_to_sort[i][1]);
 				}
-				cout << "point5\n";
+//				cout << "point5\n";
 				execl("bin/sort", "sort");
 				cout << strerror(errno) << "\n";
 				return -1;
 		
 			default:	//parent process
-				cout << "point6\n";
+//				cout << "point6\n";
 				close(pipes_to_sort[i][1]);
 				close(pipes_to_parent[i][0]);
 				break;
@@ -105,7 +110,7 @@ int main (int argc, char * argv[])
 	}
 	write_to_sort(pipes_to_sort, word_vec);
 
-	
+	read_from_sort(pipes_to_parent, num_of_proc);
 		
 	return 0;
 }
@@ -127,14 +132,14 @@ int parse_words(vector< vector<string> > &temp_word_vec, int num_of_sub_lists)
 }
 
 
-int write_to_sort(fd_t pipes_to_sort[][2], vector< vector<string> > &temp_word_vec)
+int write_to_sort(fd_t pipes_to_sort[][2], vector< vector<string> > temp_word_vec)
 {
 	cout << "point8\n";
 	FILE *pipe_write_to_sort[temp_word_vec.size()];
 	int num_words_read;
 	int total_words = 0;
 	cout << "point9\n";
-
+	
 	for(int i = 0; i < temp_word_vec.size(); i++)
 	{
 		for(int j = 0; j < temp_word_vec[i].size(); j++)
@@ -142,28 +147,82 @@ int write_to_sort(fd_t pipes_to_sort[][2], vector< vector<string> > &temp_word_v
 			total_words++;
 		}
 	}
-	
-	cout << "total words: " << total_words;
-	
+		
 	for(int i = 0; i < temp_word_vec.size(); i++)
 	{
-		pipe_write_to_sort[i] = fdopen(pipes_to_sort[i][0], "w");
+		cout << "point21\n";
+
+		pipe_write_to_sort[i] = fdopen(pipes_to_sort[i][0], "r");
+		
 	}
 	
+	cout << "point22\n";
+
 	for(int i = 0; num_words_read < total_words; i++)
 	{
+		cout << "point23\n";
+
 		for (int j = 0; j < temp_word_vec.size(); j++)
 		{
+			cout << "point24\n";
 			fputs(temp_word_vec[j][i].c_str(), pipe_write_to_sort[j]);
+			cout << "point25\n";
 			num_words_read++;
+			cout << "point26\n";
+			fflush(pipe_write_to_sort[j]);
+			cout << "point27\n";
 		}
 	}
-	
+		
 	return 0;
 }
 
 
+int read_from_sort(fd_t pipes_to_parent[][2], int num_of_pipes)
+{
+	cout << "point10\n";
+	vector< vector<string> > sorted_word_vecs;
+	FILE *pipe_read_from_sort[num_of_pipes];
+	char word_read[MAX_WORD_LENGTH];
+	string word;
+	cout << "point11\n";
 
+	for (int i = 0; i < num_of_pipes; i++)
+	{
+
+		pipe_read_from_sort[i] = fdopen(pipes_to_parent[i][1], "w");
+		
+	}
+	cout << "point12\n";
+
+	for(int i = 0; i < num_of_pipes; i++)
+	{
+		cout << "point13\n";
+		while (fgets(word_read, MAX_WORD_LENGTH, pipe_read_from_sort[i]) != NULL);
+		{
+			cout << "point18\n";
+			word = word_read;
+			cout << "point19\n";
+			sorted_word_vecs[i].push_back(word);
+			cout << "point20\n";
+		}
+		cout << "point14\n";
+	}
+	cout << "point15\n";
+
+	for (int i = 0; i < sorted_word_vecs.size(); i++)
+	{
+		cout << "point16\n";
+		for(int j = 0; j < sorted_word_vecs[i].size(); j++)
+		{
+			cout << "point17\n";
+			cout << sorted_word_vecs[i][j] << "\n";
+		}
+	}
+		 
+	
+	return 0;
+}
 
 
 
